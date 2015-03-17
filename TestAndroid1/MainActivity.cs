a6d7b20b9f66fb7d68ac839563a44a84bbbbf54d
360_macmini <saurabhd@360logica.com>
2015-03-17 17:08:47 +0530

    HelloCommit

--- TestAndroid1/MainActivity.cs
+++ TestAndroid1/MainActivity.cs
@@ -24,7 +24,7 @@ namespace TestAndroid1
 			// Get our button from the layout resource,
 			// and attach an event to it
 			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
+			//Hello
 			button.Click += delegate {
 				button.Text = string.Format ("{0} clicks!", count++);
 			};
